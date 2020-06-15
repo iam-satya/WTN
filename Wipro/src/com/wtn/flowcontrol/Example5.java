6f3eb1eba8c5a62b167122c032473d0a0036e36d
user <user@DESKTOP-MD6G5O7>
2020-06-15 07:18:20 +0530

    flowcontrol

--- Wipro/src/com/wtn/flowcontrol/Example5.java
+++ Wipro/src/com/wtn/flowcontrol/Example5.java
@@ -1,5 +1,23 @@
 package com.wtn.flowcontrol;
 
 public class Example5 {
+	public static void main(String[] args) {
+		
+	
+	
+	char value = args[0].charAt(0);
+	
+	if(Character.isDigit(value))
+		System.out.println("Digit");
+	else if(Character.isAlphabetic(value))
+		System.out.println("Alphabetic");
+	else
+		System.out.println("Special Character");
+			
+		
+	
+
+
 
 }
+	}
